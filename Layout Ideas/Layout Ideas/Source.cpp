288dd5fa369b770ce95628c7e3d81072700e6d36
ShawnDH <37811310+ShawnDH@users.noreply.github.com>
2018-05-10 13:34:47 -0500

    update Final Project

---
+++ Layout Ideas/Layout Ideas/Source.cpp
@@ -0,0 +1,15 @@
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int it;
+	cout << "Cain:  ;lkja;lsjdfjiancvj;aei;3ij;lkdjfacnv9a;efj;ahfda'c;hSDJ" << endl;
+	cout << "\nOptions  \n 1. lkjlj  \n 2. ;akjsdflkaj  \n 3. afjlaksjfoij  \n 4. alsdjfoiajn  \n 5. ajfljaijn \n >";
+	cin >> it;
+
+
+	system("pause");
+	return 0;
+}
